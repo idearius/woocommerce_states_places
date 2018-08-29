c475d4272cded5cc81b9c470166289e1a4d63bac
idearius <info@idearius.com>
2018-08-29 02:37:32 -0300

    Create UY.php

---
+++ tags/1.1.1/states/UY.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Uruguayan states
+ *
+ * @author  Alfonso Frachelle <info@idearius.com> | * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states ['UY' ] = array (
+	'UY-AR'   => 'Departamento: Artigas',
+	'UY-CA'   => 'Departamento: Canelones',
+	'UY-CL'   => 'Departamento: Cerro Largo',
+	'UY-CO'   => 'Departamento: Colonia',
+	'UY-DU'   => 'Departamento: Durazno',
+	'UY-FS'   => 'Departamento: Flores',
+	'UY-FD'   => 'Departamento: Florida',
+	'UY-LA'   => 'Departamento: Lavalleja',
+	'UY-MA'   => 'Departamento: Maldonado',
+	'UY-MO'   => 'Departamento: Montevideo',
+	'UY-PA'   => 'Departamento: Paysandú',
+	'UY-RN'   => 'Departamento: Río Negro',
+	'UY-RV'   => 'Departamento: Rivera',
+	'UY-RO'   => 'Departamento: Rocha',
+	'UY-SA'   => 'Departamento: Salto',
+	'UY-SJ'   => 'Departamento: San José',
+	'UY-SO'   => 'Departamento: Soriano',
+	'UY-TA'   => 'Departamento: Tacuarembó',
+	'UY-TT'   => 'Departamento: Treinta y Tres',
+);
